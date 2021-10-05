74b9fac4c626726fd00911767f60f7772b70d5ad
Jason Dent <jason@streetsidesoftware.nl>
2021-10-05 14:52:14 +0200

    Add sample code

---
+++ src/test.ts
@@ -0,0 +1,9 @@
+/**
+ * Sample class
+ */
+export class MyClass {
+    // Type the text in the comment below. Pause at the `>`
+    // private known = new Map<string, string>();
+    private resolvedWords = new Map<string, string>();
+    constructor(readonly words: string[]) {}
+}
